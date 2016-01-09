b96d7b522989ca62fe4052108966365001443dfe
Kashav Madan <kshvmdn@gmail.com>
2016-01-09 17:35:03 -0500

    Add pagination

--- app/controllers/dataController.js
+++ app/controllers/dataController.js
@@ -1,11 +1,18 @@
 angular.module('app').controller('DataCtrl', function($scope, $http) {
+
+  $scope.currentPage = 1;
+  $scope.numPerPage = 10;
+  $scope.maxSize = 5;
+
   var url = "http://www1.toronto.ca/transportation/roadrestrictions/RoadRestrictions.json";
+
   $http.get(url).success(function(data) {
     json = eval(data); // data is JSONP, presented with callback jsonClosureCallback()
-    $scope.data = json;
+    $scope.closures = json['Closure'];
     console.log(json);
   }).error(function(data) {
-    $scope.data = "Request failed";
+    $scope.closures = {};
+    console.log('Request failed')
   });
   var jsonClosureCallback = function(data) { return data; }
 });
